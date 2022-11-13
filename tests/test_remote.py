2caa00de5abab41aecd223be91cd33e8fe27ede0
tornikeo <tornikeonoprishvili@gmail.com>
2022-11-13 15:55:37 +0400

    Test model works

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -3,8 +3,24 @@
 
 import requests
 import time
-model_inputs = {'image': 'https://i.imgur.com/IdcBNz7.jpg'}
-res = requests.post('http://localhost:9000/', json = model_inputs)
+import utils
+import tempfile
+from pathlib import Path
+
+while True:
+    try:
+        for infile in Path('./tests/data/').glob('*'):
+            print('input file', infile)
+            model_inputs = {'image_base64': utils.read_b64(infile)}
+            res = requests.post('http://localhost:9000/', json = model_inputs)
+            ofile = Path('./tests/out/') / infile.name
+            utils.write_b64(res.json()['image_base64'], ofile)
+            print('output file', ofile)
+            input("Continue?")
+    except:
+        time.sleep(.5)
+
+
 print(res.json())
 
 # from main_test_swinir import main
